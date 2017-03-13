c5c334a0cb7b1e3f29a0720556d8ea961c4107d3
ConchobarMacNessa <alice.binghamcarr@gmail.com>
2017-03-13 15:03:57 +0000

    Serve html file

--- server.js
+++ server.js
@@ -7,26 +7,23 @@ var message2 = 'You are on the girls page!';
 function handler (request, response) {
 
   var endpoint = request.url;
-  console.log(endpoint);
-
   var method = request.method;
-  console.log(method);
 
-  if (endpoint === "/node") {
+  if (endpoint === "/") {
     response.writeHead(200, {"Content-Type": "text/html"});
-    response.write(message1); //response body
-    response.end(); //finish response
-  }
 
-  else if (endpoint === "/girls") {
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.write(message2); //response body
-    response.end(); //finish response
-  }
+    fs.readFile(__dirname + '/public/index.html', function(error, file) {
+      if (error) {
+        console.log(error);
+        return;
+      }
 
+      response.end(file);
+    });
+  }
 }
 
-var server = http.createServer(handler        );
+var server = http.createServer(handler);
 server.listen(3000, function(){
   console.log('Server is listening on port 3000.  Ready to accept requests!');
 
