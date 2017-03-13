d42ba44b28a11995e222d2cb4c664fd355c28999
ConchobarMacNessa <alice.binghamcarr@gmail.com>
2017-03-13 14:59:07 +0000

    Add different endpoints

--- src/server.js
+++ src/server.js
@@ -1,14 +1,32 @@
 var http = require('http');
+var fs = require('fs');
 
-var message = 'I am so happy to be part of the Node Girls workshop!';
+var message1 = 'I am so happy to be part of the Node Girls workshop!';
+var message2 = 'You are on the girls page!';
 
 function handler (request, response) {
-  response.writeHead(200, {"Content-Type": "text/html"});
-  response.write(message); //response body
-  response.end(); //finish response
+
+  var endpoint = request.url;
+  console.log(endpoint);
+
+  var method = request.method;
+  console.log(method);
+
+  if (endpoint === "/node") {
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(message1); //response body
+    response.end(); //finish response
+  }
+
+  else if (endpoint === "/girls") {
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(message2); //response body
+    response.end(); //finish response
+  }
+
 }
 
-var server = http.createServer(handler);
+var server = http.createServer(handler        );
 server.listen(3000, function(){
   console.log('Server is listening on port 3000.  Ready to accept requests!');
 
